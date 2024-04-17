182e217bde8198014a9ee8cd19fadd70b721ead4
emilylin1 <lin.emi@northeastern.edu>
2024-04-16 22:56:49 -0400

    a6

---
+++ Kanbas/courses/schema.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+const coursesSchema = new mongoose.Schema({
+    id: String,
+    name: String,
+    number: String,
+    startDate: Date,
+    endDate: Date,
+    department: String,
+    credits: Number,
+    description: String,
+  },
+  { collection: "courses" });
+export default coursesSchema;
