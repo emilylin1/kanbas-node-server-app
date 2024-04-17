a43ecc6c77383a110dbaba78fdc4c3b10e4fd69b
emilylin1 <lin.emi@northeastern.edu>
2024-04-16 23:00:29 -0400

    deleted old code

--- Kanbas/modules/routes.js
+++ Kanbas/modules/routes.js
@@ -24,37 +24,5 @@ function ModuleRoutes(app) {
   app.delete("/api/modules/:moduleId", deleteModule);
   app.post("/api/courses/:courseId/modules", createModule);
   app.get("/api/courses/:courseId/modules", findModulesForCourse);
-
-  // app.put("/api/modules/:mid", (req, res) => {
-  //     const { mid } = req.params;
-  //     const moduleIndex = db.modules.findIndex(
-  //         (m) => m._id === mid);
-  //     db.modules[moduleIndex] = {
-  //         ...db.modules[moduleIndex],
-  //         ...req.body
-  //     };
-  //     res.sendStatus(204);
-  // });
-  // app.delete("/api/modules/:mid", (req, res) => {
-  //     const { mid } = req.params;
-  //     db.modules = db.modules.filter((m) => m._id !== mid);
-  //     res.sendStatus(200);
-  // });
-  // app.post("/api/courses/:cid/modules", (req, res) => {
-  //     const { cid } = req.params;
-  //     const newModule = {
-  //         ...req.body,
-  //         course: cid,
-  //         _id: new Date().getTime().toString(),
-  //     };
-  //     db.modules.push(newModule);
-  //     res.send(newModule);
-  // });
-  // app.get("/api/courses/:cid/modules", (req, res) => {
-  //     const { cid } = req.params;
-  //     const modules = db.modules
-  //         .filter((m) => m.course === cid);
-  //     res.send(modules);
-  // });
 }
 export default ModuleRoutes;
